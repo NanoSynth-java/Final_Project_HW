02f76c6821dd4add8bd24b1f38abd3c8f9b3e331
Daniil <Comm2k3@hotmail.com>
2023-01-30 03:59:40 +0200

    done

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string[] people = {"John","Sam","Laury"};
+string[] nju = new string[3];
+int size = people.Length;
+for (int i = 0; i < size; i++)
+{
+    var len = people[i].Length;
+    if (len <= 3)
+    {
+        nju[i] = people[i];
+    }
+}
+    Console.WriteLine($"va2: {nju[0]}, {nju[1]}, {nju[2]}");
